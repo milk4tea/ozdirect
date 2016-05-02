994b4f972a93e91199072f8241de314431986ed6
root <root@oznutrition.net>
2016-05-02 14:57:26 +1000

    add files

---
+++ app/etc/env.php
@@ -0,0 +1,58 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin_1311',
+  ),
+  'install' => 
+  array (
+    'date' => 'Tue, 26 Apr 2016 11:22:16 +0000',
+  ),
+  'crypt' => 
+  array (
+    'key' => '461b7925578e8b8b8e5a3ae92f3b0686',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'oz-nutrition-mysql.cz9w9hjynm8m.ap-southeast-2.rds.amazonaws.com:3306',
+        'dbname' => 'oznutrition',
+        'username' => 'root',
+        'password' => 'IBvAUq1Wg#qp',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 0,
+    'layout' => 0,
+    'block_html' => 0,
+    'collections' => 0,
+    'reflection' => 0,
+    'db_ddl' => 0,
+    'eav' => 0,
+    'config_integration' => 0,
+    'config_integration_api' => 0,
+    'full_page' => 0,
+    'translate' => 0,
+    'config_webservice' => 0,
+  ),
+);
